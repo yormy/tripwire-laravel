65c8ef9504a482950dd45a2d6fa60983ed4c772d
Mexion <dev@mexion.com>
2023-05-30 21:03:28 +0200

    Config object abstraction

---
+++ src/Http/Middleware/Config.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Yormy\TripwireLaravel\Http\Middleware;
+
+use Illuminate\Http\Request;
+
+class Config
+{
+    public bool $enabled;
+    public array $methods;
+
+    public array $routes;
+
+    public array $inputs;
+
+    public array $autoBlocks;
+
+    public array $words;
+
+    public function __construct(string $checker)
+    {
+        $data = config('tripwire.middleware.' . $checker);
+
+        $this->enabled = $data['enabled'] ?? $this->tripwireEnabled();
+        $this->methods = $data['methods'];
+        $this->routes = $data['routes'];
+        $this->inputs = $data['inputs'];
+        $this->autoBlocks = $data['auto_block'];
+        $this->words = $data['words'];
+
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    public function isDisabled(): bool
+    {
+        return !$this->enabled;
+    }
+
+    public function skipMethod(Request $request): bool
+    {
+        if ( !$this->methods) {
+            return true;
+        }
+
+        if (in_array('all', $this->methods)) {
+            return false;
+        }
+
+        return !in_array(strtolower($request->method()), $this->methods);
+    }
+
+
+    public function skipRoute(Request $request): bool
+    {
+        if ( !$this->routes) {
+            return false;
+        }
+
+        foreach ($this->routes['except'] as $ex) {
+            if (! $request->is($ex)) {
+                continue;
+            }
+
+            return true;
+        }
+
+        foreach ($this->routes['only'] as $on) {
+            if ($request->is($on)) {
+                continue;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function tripwireEnabled(): bool
+    {
+        return config('tripwire.enabled', true);
+    }
+}
