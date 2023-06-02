e1e91a14210be3f38ad4390039cfa0b77c611f42
Mexion <dev@mexion.com>
2023-06-02 14:27:51 +0200

    abstracted block if needed

--- src/Http/Middleware/Checkers/Sqli.php
+++ src/Http/Middleware/Checkers/Sqli.php
@@ -4,84 +4,18 @@ namespace Yormy\TripwireLaravel\Http\Middleware\Checkers;
 
 use Yormy\TripwireLaravel\Http\Middleware\Middleware;
 use Yormy\TripwireLaravel\Observers\Events\SwearFailedEvent;
-use Yormy\TripwireLaravel\Repositories\BlockRepository;
-use Yormy\TripwireLaravel\Repositories\LogRepository;
 
 class Sqli extends Middleware
 {
 
     protected function attackFound(array $violations): void
     {
-        $attackScore = $this->getAttackScore();
-
         event(new SwearFailedEvent(
-            attackScore: $attackScore,
+            attackScore: $this->getAttackScore(),
             violations: $violations
         ));
 
         $this->blockIfNeeded();
-
     }
-
-    protected function blockIfNeeded()
-    {
-        $logRepository = new LogRepository();
-
-        $punishableTimeframe = (int)$this->config->punish->withinMinutes;
-
-        $ipAddressClass = config('tripwire.services.ip_address');
-        $ipAddress = $ipAddressClass::get($this->request);
-
-        $violationsByIp = $logRepository->queryViolationsByIp($punishableTimeframe, ['test', 'SWEAR'], $ipAddress);
-        $scoreByIp = $violationsByIp->get()->sum('event_score');
-
-        $userClass = config('tripwire.services.user');
-        $userId = $userClass::getId($this->request);
-        $userType = $userClass::getType($this->request);
-        $scoreByUser = 0;
-
-        $violationsByUser = null;
-        if ($userId) {
-            $violationsByUser = $logRepository->queryViolationsByUser($punishableTimeframe, ['test', 'SWEAR'],  $userId, $userType);
-            $scoreByUser = $violationsByUser->get()->sum('event_score');
-        }
-
-        $requestSourceClass = config('tripwire.services.request_source');
-        $browserFingerprint =$requestSourceClass::getBrowserFingerprint();
-        $scoreByBrowser = 0;
-        $violationsByBrowser = null;
-        if ($browserFingerprint) {
-            $violationsByBrowser = $logRepository->queryViolationsByBrowser($punishableTimeframe, ['test', 'SWEAR'],  $browserFingerprint);
-            $scoreByBrowser = $violationsByBrowser->get()->sum('event_score');
-        }
-
-        $maxScore = max($scoreByIp, $scoreByUser, $scoreByBrowser);
-
-        if ($maxScore > (int)$this->config->punish->score) {
-            $blockRepository = new BlockRepository();
-            $blockItem = $blockRepository->add(
-                penaltySeconds: (int)$this->config->punish->penaltySeconds,
-                ipAddress: $ipAddress,
-                userId: $userId,
-                userType: $userType,
-                browserFingerprint: $browserFingerprint,
-            );
-
-            $violationsByIp->update(['tripwire_block_id' => $blockItem->id]);
-            if (!$violationsByUser) {
-                $violationsByUser->update(['tripwire_block_id' => $blockItem->id]);
-            }
-
-            if ($violationsByBrowser) {
-                $violationsByBrowser->update(['tripwire_block_id' => $blockItem->id]);
-            }
-
-            return $blockItem;
-        }
-
-        return null;
-    }
-
-
 
 }
--- src/Http/Middleware/Middleware.php
+++ src/Http/Middleware/Middleware.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 use Yormy\TripwireLaravel\DataObjects\ConfigResponse;
+use Yormy\TripwireLaravel\Repositories\BlockRepository;
+use Yormy\TripwireLaravel\Repositories\LogRepository;
 use Yormy\TripwireLaravel\Services\ResponseDeterminer;
 use Yormy\TripwireLaravel\DataObjects\ConfigMiddleware;
 use Yormy\TripwireLaravel\Services\Routes;
@@ -158,6 +160,66 @@ abstract class Middleware
         }
 
         return $result;
+    }
+
+
+    protected function blockIfNeeded()
+    {
+        $logRepository = new LogRepository();
+
+        $punishableTimeframe = (int)$this->config->punish->withinMinutes;
+
+        $ipAddressClass = config('tripwire.services.ip_address');
+        $ipAddress = $ipAddressClass::get($this->request);
+
+        $violationsByIp = $logRepository->queryViolationsByIp($punishableTimeframe, ['test', 'SWEAR'], $ipAddress);
+        $scoreByIp = $violationsByIp->get()->sum('event_score');
+
+        $userClass = config('tripwire.services.user');
+        $userId = $userClass::getId($this->request);
+        $userType = $userClass::getType($this->request);
+        $scoreByUser = 0;
+
+        $violationsByUser = null;
+        if ($userId) {
+            $violationsByUser = $logRepository->queryViolationsByUser($punishableTimeframe, ['test', 'SWEAR'],  $userId, $userType);
+            $scoreByUser = $violationsByUser->get()->sum('event_score');
+        }
+
+        $requestSourceClass = config('tripwire.services.request_source');
+        $browserFingerprint =$requestSourceClass::getBrowserFingerprint();
+        $scoreByBrowser = 0;
+        $violationsByBrowser = null;
+        if ($browserFingerprint) {
+            $violationsByBrowser = $logRepository->queryViolationsByBrowser($punishableTimeframe, ['test', 'SWEAR'],  $browserFingerprint);
+            $scoreByBrowser = $violationsByBrowser->get()->sum('event_score');
+        }
+
+        $maxScore = max($scoreByIp, $scoreByUser, $scoreByBrowser);
+
+        if ($maxScore > (int)$this->config->punish->score) {
+            $blockRepository = new BlockRepository();
+            $blockItem = $blockRepository->add(
+                penaltySeconds: (int)$this->config->punish->penaltySeconds,
+                ipAddress: $ipAddress,
+                userId: $userId,
+                userType: $userType,
+                browserFingerprint: $browserFingerprint,
+            );
+
+            $violationsByIp->update(['tripwire_block_id' => $blockItem->id]);
+            if (!$violationsByUser) {
+                $violationsByUser->update(['tripwire_block_id' => $blockItem->id]);
+            }
+
+            if ($violationsByBrowser) {
+                $violationsByBrowser->update(['tripwire_block_id' => $blockItem->id]);
+            }
+
+            return $blockItem;
+        }
+
+        return null;
     }
 
     public function prepareInput($value)
