793a884c0cc3ea2fef6ff80fbd707a7ecb59dc0a
Mexion <dev@mexion.com>
2023-06-02 09:39:15 +0200

    Show blocked page

--- resources/views/blocked.blade.php
+++ resources/views/blocked.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Oeps</title>
+    <title>Oops</title>
 
     <style id="" media="all">/* cyrillic-ext */
         * {
