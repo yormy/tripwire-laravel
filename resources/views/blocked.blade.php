582d3ccf142602479f42016ba59e023a3d78cff3
Mexion <dev@mexion.com>
2023-06-02 09:36:21 +0200

    Blocked page design

--- resources/views/blocked.blade.php
+++ resources/views/blocked.blade.php
@@ -6,119 +6,169 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>404 HTML Template by Colorlib</title>
+    <title>Oeps</title>
 
     <style id="" media="all">/* cyrillic-ext */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459WRhyzbi.woff2) format('woff2');
-            unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
-        }
-        /* cyrillic */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459W1hyzbi.woff2) format('woff2');
-            unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
-        }
-        /* vietnamese */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459WZhyzbi.woff2) format('woff2');
-            unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+0300-0301, U+0303-0304, U+0308-0309, U+0323, U+0329, U+1EA0-1EF9, U+20AB;
-        }
-        /* latin-ext */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459Wdhyzbi.woff2) format('woff2');
-            unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-        }
-        /* latin */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-        }
-        /* cyrillic-ext */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 900;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459WRhyzbi.woff2) format('woff2');
-            unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
-        }
-        /* cyrillic */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 900;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459W1hyzbi.woff2) format('woff2');
-            unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
-        }
-        /* vietnamese */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 900;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459WZhyzbi.woff2) format('woff2');
-            unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+0300-0301, U+0303-0304, U+0308-0309, U+0323, U+0329, U+1EA0-1EF9, U+20AB;
-        }
-        /* latin-ext */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 900;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459Wdhyzbi.woff2) format('woff2');
-            unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-        }
-        /* latin */
-        @font-face {
-            font-family: 'Montserrat';
-            font-style: normal;
-            font-weight: 900;
-            font-display: swap;
-            src: url(/fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        * {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box
         }
 
-        *{-webkit-box-sizing:border-box;box-sizing:border-box}body{padding:0;margin:0}#notfound{position:relative;height:100vh}#notfound .notfound-bg{position:absolute;width:100%;height:100%;background-image:url(../img/bg.jpg);background-size:cover}#notfound .notfound-bg:after{content:'';position:absolute;width:100%;height:100%;background-color:rgba(255,0,36,.7)}#notfound .notfound{position:absolute;left:50%;top:50%;-webkit-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);transform:translate(-50%,-50%)}.notfound{max-width:910px;width:100%;line-height:1.4;text-align:center}.notfound .notfound-404{position:relative;height:200px}.notfound .notfound-404 h1{font-family:montserrat,sans-serif;position:absolute;left:50%;top:50%;-webkit-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);transform:translate(-50%,-50%);font-size:220px;font-weight:900;margin:0;color:#fff;text-transform:uppercase;letter-spacing:10px}.notfound h2{font-family:montserrat,sans-serif;font-size:22px;font-weight:700;text-transform:uppercase;color:#fff;margin-top:20px;margin-bottom:15px}.notfound .home-btn,.notfound .contact-btn{font-family:montserrat,sans-serif;display:inline-block;font-weight:700;text-decoration:none;background-color:transparent;border:2px solid transparent;text-transform:uppercase;padding:13px 25px;font-size:18px;border-radius:40px;margin:7px;-webkit-transition:.2s all;transition:.2s all}.notfound .home-btn:hover,.notfound .contact-btn:hover{opacity:.9}.notfound .home-btn{color:rgba(255,0,36,.7);background:#fff}.notfound .contact-btn{border:2px solid rgba(255,255,255,.9);color:rgba(255,255,255,.9)}.notfound-social{margin-top:25px}.notfound-social>a{display:inline-block;height:40px;line-height:40px;width:40px;font-size:14px;color:rgba(255,255,255,.9);margin:0 6px;-webkit-transition:.2s all;transition:.2s all}.notfound-social>a:hover{color:rgba(255,0,36,.7);background-color:#fff;border-radius:50%}@media only screen and (max-width:767px){.notfound .notfound-404 h1{font-size:182px}}@media only screen and (max-width:480px){.notfound .notfound-404{height:146px}.notfound .notfound-404 h1{font-size:146px}.notfound h2{font-size:16px}.notfound .home-btn,.notfound .contact-btn{font-size:14px}}
+        body {
+            padding: 0;
+            margin: 0
+        }
+
+        #blocked {
+            position: relative;
+            height: 100vh
+        }
+
+        #blocked .blocked-bg {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-image: url(../img/bg.jpg);
+            background-size: cover
+        }
+
+        #blocked .blocked-bg:after {
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(255, 0, 36, .7)
+        }
+
+        #blocked .blocked {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%)
+        }
+
+        .blocked {
+            max-width: 910px;
+            width: 100%;
+            line-height: 1.4;
+            text-align: center
+        }
+
+        .blocked .blocked-title {
+            position: relative;
+            height: 200px
+        }
+
+        .blocked .blocked-title h1 {
+            font-family: montserrat, sans-serif;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            font-size: 220px;
+            font-weight: 900;
+            margin: 0;
+            color: #fff;
+            text-transform: uppercase;
+            letter-spacing: 10px
+        }
+
+        .blocked h2 {
+            font-family: montserrat, sans-serif;
+            font-size: 22px;
+            font-weight: 700;
+            text-transform: uppercase;
+            color: #fff;
+            margin-top: 20px;
+            margin-bottom: 15px
+        }
+
+        .blocked .primary,
+        .blocked .secondary {
+            font-family: montserrat, sans-serif;
+            display: inline-block;
+            font-weight: 700;
+            text-decoration: none;
+            background-color: transparent;
+            border: 2px solid transparent;
+            text-transform: uppercase;
+            padding: 13px 25px;
+            font-size: 18px;
+            border-radius: 40px;
+            margin: 7px;
+            -webkit-transition: .2s all;
+            transition: .2s all
+        }
+
+        .blocked .primary:hover,
+        .blocked .secondary:hover {
+            opacity: .9
+        }
+
+        .blocked .primary {
+            color: rgba(255, 0, 36, .7);
+            background: #fff
+        }
+
+        .blocked .secondary {
+            border: 2px solid rgba(255, 255, 255, .9);
+            color: rgba(255, 255, 255, .9)
+        }
+
+        .blocked-social>a {
+            display: inline-block;
+            height: 40px;
+            line-height: 40px;
+            width: 40px;
+            font-size: 14px;
+            color: rgba(255, 255, 255, .9);
+            margin: 0 6px;
+            -webkit-transition: .2s all;
+            transition: .2s all
+        }
+
+        .blocked-social>a:hover {
+            color: rgba(255, 0, 36, .7);
+            background-color: #fff;
+            border-radius: 50%
+        }
+
+        @media only screen and (max-width:767px) {
+            .blocked .blocked-title h1 {
+                font-size: 182px
+            }
+        }
+
+        @media only screen and (max-width:480px) {
+            .blocked .blocked-title {
+                height: 146px
+            }
+            .blocked .blocked-title h1 {
+                font-size: 146px
+            }
+            .blocked h2 {
+                font-size: 16px
+            }
+            .blocked .primary,
+            .blocked .secondary {
+                font-size: 14px
+            }
+        }
     </style>
-
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
 <body>
-<div id="notfound">
-    <div class="notfound-bg"></div>
-    <div class="notfound">
-        <div class="notfound-404">
-            <h1>404</h1>
+<div id="blocked">
+    <div class="blocked-bg"></div>
+    <div class="blocked">
+        <div class="blocked-title">
+            <h1>Oeps</h1>
         </div>
-        <h2>we are sorry, but the page you requested was not found</h2>
-        <a href="#" class="home-btn">Go Home</a>
-        <a href="#" class="contact-btn">Contact us</a>
-        <div class="notfound-social">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-pinterest"></i></a>
-            <a href="#"><i class="fa fa-google-plus"></i></a>
-        </div>
+        <h2>You have been blocked due to malicious activity</h2>
+        <a href="#" class="primary">Go Home</a>
+        <a href="#" class="secondary">Contact us</a>
     </div>
 </div>
 </body>
