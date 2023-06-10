ff29d79cc575bf78388a4ada17eefa1a8bb26fd0
Rob <rob@prigroup.eu>
2023-06-10 17:30:39 +0200

    test responses json

--- _docs/TODO.md
+++ _docs/TODO.md
@@ -30,13 +30,14 @@ Specify type of block view/json:  violation is anders dan login throttle
 # Unit test cases
 test every type of response, code, exception, view, etc at least once
 test default fallback responses
-test son
+test json
 
     /*
         if ($data['json'] ?? false) {
             $this->json = $data['json'];
         }
      */
+test all checkers? => data provider
 
 import akunetics tests
 
---
+++ src/Tests/Feature/ResponsesJsonTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Yormy\TripwireLaravel\Tests\Feature;
+
+use Yormy\TripwireLaravel\Exceptions\TripwireFailedException;
+use Yormy\TripwireLaravel\Http\Middleware\Checkers\Text;
+use Yormy\TripwireLaravel\Models\TripwireLog;
+use Yormy\TripwireLaravel\Tests\TestCase;
+
+class ResponsesJsonTest extends TestCase
+{
+    private string $tripwire ='text';
+    const HTTP_TRIPWIRE_CODE = 409;
+    /**
+     * @test
+     */
+    public function json_respond_as_exception_expects_exception()
+    {
+        $this->setConfig(["exception" => TripwireFailedException::class]);
+
+        $this->expectException(TripwireFailedException::class);
+
+        $this->triggerTripwire();
+    }
+
+    /**
+     * @test
+     */
+    public function json_respond_as_message_expects_message()
+    {
+        $messageKey = "message.key";
+        $this->setConfig(["message_key" => $messageKey]);
+
+        $startCount = TripwireLog::count();
+
+        $result = $this->triggerTripwire();
+
+        $this->assertLogAddedToDatabase($startCount);
+
+        $this->assertEquals($result->getOriginalContent(), $messageKey);
+    }
+
+
+    /**
+     * @test
+     */
+    public function json_respond_as_json_expects_json()
+    {
+        $json =  ['data' => 'somedata', 'err' =>'2'];
+        $this->setConfig(["json" => $json]);
+
+        $startCount = TripwireLog::count();
+
+        $result = $this->triggerTripwire();
+
+        $this->assertLogAddedToDatabase($startCount);
+
+        $this->assertEquals($result->getOriginalContent(), $json);
+    }
+
+    private function triggerTripwire()
+    {
+        $request = $this->app->request; // default is as HTML
+        $request->query->set('foo', 'aaa');
+        $request->headers->set('Accept', 'application/json');
+
+        return (new Text($request))->handle($request, $this->getNextClosure());
+    }
+
+    private function assertLogAddedToDatabase($startCount)
+    {
+        $this->assertGreaterThan($startCount, TripwireLog::count());
+    }
+
+    private function setConfig(array $data)
+    {
+        config(["tripwire_wires.$this->tripwire.trigger_response.json" => $data]);
+    }
+}
