dddc2e679c907c1a37172979fe3615f3ad80edaf
Rob <rob@prigroup.eu>
2023-06-11 08:58:46 +0200

    add lfi test

---
+++ src/Tests/Feature/LfiTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Yormy\TripwireLaravel\Tests\Feature;
+
+use Yormy\TripwireLaravel\Http\Middleware\Checkers\Lfi;
+
+class LfiTest extends BaseMiddlewareTester
+{
+    protected string $tripwire ='lfi';
+
+    protected $tripwireClass = Lfi::class;
+
+    protected array $accepting = [
+        'dsfsdf',
+        'sss',
+    ];
+
+    protected array $violations = [
+        "../../../../etc/passwd",
+    ];
+}
