8e41be9ab365875f4bcf1efb29f6234c84ffc1e3
Rob <rob@prigroup.eu>
2023-06-16 07:39:41 +0200

    abstracted extended tests

--- src/Tests/Extensive/Middleware/Checkers/XssExtensiveTest.php
+++ src/Tests/Extensive/Middleware/Checkers/XssExtensiveTest.php
@@ -3,40 +3,18 @@
 namespace Yormy\TripwireLaravel\Tests\Extensive\Middleware\Checkers;
 
 use Yormy\TripwireLaravel\Http\Middleware\Checkers\Xss;
-use Yormy\TripwireLaravel\Models\TripwireLog;
-use Yormy\TripwireLaravel\Tests\TestCase;
-use Yormy\TripwireLaravel\Tests\Traits\TripwireTestTrait;
 
-class XssExtensiveTest extends TestCase
+class XssExtensiveTest extends BaseExtensive
 {
-    use TripwireTestTrait;
-
-    protected string $tripwire ='xss';
-
     protected $tripwireClass = Xss::class;
 
+    protected string $violationsDataFile = './src/Tests/Dataproviders/XssViolationsData.txt';
+
     protected array $accepting = [
-        'dsfsdf',
-        'sss',
+        'saaaaaaa',
     ];
 
     protected array $violations;
-
-    public function __construct(?string $name = null, array $data = [], $dataName = '')
-    {
-        $this->violations = file('./src/Tests/Dataproviders/XssViolationsData.txt');
-
-        // ignore commented out with #
-        foreach ($this->violations as $index => $violation) {
-            if(str_starts_with($violation, '##')) {
-                unset ($this->violations[$index]);
-            }
-        }
-
-        parent::__construct($name, $data, $dataName);
-
-
-    }
 
     /**
      * @test
@@ -45,15 +23,7 @@ class XssExtensiveTest extends TestCase
      */
     public function should_accept(string $accept)
     {
-        $this->setConfig();
-
-        $startCount = TripwireLog::count();
-
-        $result = $this->triggerTripwire($accept);
-
-        $this->assertNotLogged($startCount);
-
-        $this->assertEquals('next', $result);
+        $this->assertAccept($accept);
     }
 
     /**
@@ -63,15 +33,6 @@ class XssExtensiveTest extends TestCase
      */
     public function should_block(string $violation)
     {
-        $this->setConfig();
-
-        $startCount = TripwireLog::count();
-
-        $result = $this->triggerTripwire($violation);
-
-        $this->assertNotEquals('next', $result, 'Should block but did not');
-        $this->assertEquals(409, $result->getStatusCode());
-
-        $this->assertLogAddedToDatabase($startCount);
+        $this->assertBlock($violation);
     }
 }
