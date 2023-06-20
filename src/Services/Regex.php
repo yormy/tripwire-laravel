6356e05119042a6322b7ddfa7541d94f2f213ad5
Rob <rob@prigroup.eu>
2023-06-20 18:52:22 +0200

    add const helpers

--- src/Services/Regex.php
+++ src/Services/Regex.php
@@ -4,6 +4,10 @@ namespace Yormy\TripwireLaravel\Services;
 
 class Regex
 {
+    const FILLER = '(\s|\x00)';
+
+    const QUOTE = '("|\')';
+
     public static function forbidden(array $signatures, string $delim = '#'): string
     {
         return $delim.'('.self::or($signatures).')'.$delim.'iUu';
