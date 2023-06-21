3e9ae430c41fc4e61374cf4d0f01ba31f686fff2
Rob <rob@prigroup.eu>
2023-06-21 16:08:20 +0200

    generate acceptsData

---
+++ src/Console/Commands/GenerateAccepts.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Yormy\TripwireLaravel\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class GenerateAccepts extends Command
+{
+    protected $signature = 'generate:accepts';
+
+    protected $description = 'Generate a file with random text that should be accepted';
+
+    public function handle()
+    {
+        $locales = [
+            'en_GB',
+            'de_DE',
+            'es_ES',
+            'ar_SA',
+            'ru_RU'
+        ];
+
+        foreach ($locales as $locale) {
+            $accepts = $this->buildAccept($locale, 500);
+            $contents = implode(PHP_EOL, $accepts);
+
+            $filename = "AcceptsData-$locale.txt";
+            Storage::disk('local')->put($filename, $contents);
+        }
+
+
+        return Command::SUCCESS;
+    }
+
+    private function buildAccept(string $locale, int $lines = 2): array
+    {
+        $accepts = [];
+        for ($i = 1; $i <= $lines; $i++) {
+            $accepts[] = $this->buildText($locale);
+        }
+
+        return $accepts;
+    }
+
+    public function buildText($locale): string
+    {
+        $name = fake($locale)->lastName();
+        $realText =fake($locale)->realText(200); // characters
+
+        return $name. '-'. $realText;
+    }
+}
