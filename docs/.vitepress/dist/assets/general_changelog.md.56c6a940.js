eb611fc9a303e0fc79f6b5864a6b7c09dab4d973
Mexion <dev@mexion.com>
2023-07-25 18:33:13 +0200

    build docs

---
+++ docs/.vitepress/dist/assets/general_changelog.md.56c6a940.js
@@ -0,0 +1,21 @@
+import{_ as a,o as s,c as n,V as e}from"./chunks/framework.5f0cae6b.js";const g=JSON.parse('{"title":"Changelog","description":"","frontmatter":{},"headers":[],"relativePath":"general/changelog.md","filePath":"general/changelog.md"}'),l={name:"general/changelog.md"},p=e(`<h1 id="changelog" tabindex="-1">Changelog <a class="header-anchor" href="#changelog" aria-label="Permalink to &quot;Changelog&quot;">​</a></h1><p>All notable changes to <code>tripwire</code> will be documented in this file</p><h2 id="unreleased" tabindex="-1">[Unreleased] <a class="header-anchor" href="#unreleased" aria-label="Permalink to &quot;[Unreleased]&quot;">​</a></h2><p>[branch]</p><ul><li>what you are currently working on</li></ul><h2 id="initial-release" tabindex="-1">[initial release] <a class="header-anchor" href="#initial-release" aria-label="Permalink to &quot;[initial release]&quot;">​</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight"><code><span class="line"><span style="color:#A6ACCD;">##################################################################</span></span>
+<span class="line"><span style="color:#A6ACCD;">########################## TEMPLATE ##############################</span></span>
+<span class="line"><span style="color:#A6ACCD;">##################################################################</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">## [Unreleased] </span></span>
+<span class="line"><span style="color:#A6ACCD;">[branch]</span></span>
+<span class="line"><span style="color:#A6ACCD;">- what you are currently working on</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">## [1.0.0] - 2020-11-24</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">### Security</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">### Fixed</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">### Added</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">### Changed</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">### Removed</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">### Depricated</span></span></code></pre></div>`,7),o=[p];function c(t,r,i,C,A,d){return s(),n("div",null,o)}const y=a(l,[["render",c]]);export{g as __pageData,y as default};
