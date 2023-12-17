b0ef9961915fb6ad8fc6685a8313d1bf01017b75
Mexion <dev@mexion.com>
2023-12-17 11:32:37 +0100

    cleanup

--- src/Http/Controllers/Resources/LogCollection.php
+++ src/Http/Controllers/Resources/LogCollection.php
@@ -8,13 +8,4 @@ class LogCollection extends ResourceCollection
 {
     public $collects = LogResource::class;
 
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     */
-    public function toArray($request): array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-    {
-        return parent::toArray($request);
-    }
 }
