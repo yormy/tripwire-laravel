f569736f6bbf4c3f10436272879aca41cb904e8a
Rob <rob@prigroup.eu>
2024-03-17 18:31:39 +0100

    phpstan : userresolver=interface

---
+++ src/Actions/Interfaces/UserResolverInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yormy\TripwireLaravel\Actions\Interfaces;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+interface UserResolverInterface
+{
+    public static function getCurrent(): ?Authenticatable;
+
+    public static function getRandom(): ?Authenticatable;
+
+    public static function getMemberById(string | int $id): Authenticatable;
+
+    public static function getAdminById(string | int $id): Authenticatable;
+
+    public static function getMemberByXid(string | int $id): Authenticatable;
+
+    public static function getAdminByXid(string | int $id): Authenticatable;
+
+    public static function getMemberOnXId(string | int $xid): ?Authenticatable;
+}
--- src/Services/Resolvers/UserResolver.php
+++ src/Services/Resolvers/UserResolver.php
@@ -4,43 +4,45 @@ declare(strict_types=1);
 
 namespace Yormy\TripwireLaravel\Services\Resolvers;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
+use Yormy\TripwireLaravel\Actions\Interfaces\UserResolverInterface;
 use Yormy\TripwireLaravel\Tests\Setup\Models\Admin;
 use Yormy\TripwireLaravel\Tests\Setup\Models\Member;
 
-class UserResolver
+class UserResolver implements UserResolverInterface
 {
-    public static function getCurrent(): Member | Admin | null
+    public static function getCurrent(): ?Authenticatable
     {
         return Auth::user();
     }
 
-    public static function getRandom(): ?Member
+    public static function getRandom(): ?Authenticatable
     {
         return Member::first();
     }
 
-    public static function getMemberById(string | int $id): Member
+    public static function getMemberById(string | int $id): Authenticatable
     {
         return Member::where('id', $id)->firstOrFail();
     }
 
-    public static function getAdminById(string | int $id): Admin
+    public static function getAdminById(string | int $id): Authenticatable
     {
         return Admin::where('id', $id)->firstOrFail();
     }
 
-    public static function getMemberByXid(string | int $id): Member
+    public static function getMemberByXid(string | int $id): Authenticatable
     {
         return Member::where('xid', $id)->firstOrFail();
     }
 
-    public static function getAdminByXid(string | int $id): Admin
+    public static function getAdminByXid(string | int $id): Authenticatable
     {
         return Admin::where('xid', $id)->firstOrFail();
     }
 
-    public static function getMemberOnXId(string | int $xid): ?Member
+    public static function getMemberOnXId(string | int $xid): ?Authenticatable
     {
         return Member::where('xid', $xid)->first();
     }
