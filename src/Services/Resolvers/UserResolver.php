2fbb4dc133fb9fec3f1139ac8871044a49a4b46b
Rob <rob@prigroup.eu>
2024-03-17 18:27:22 +0100

    phpstan : userresolver

--- src/Services/Resolvers/UserResolver.php
+++ src/Services/Resolvers/UserResolver.php
@@ -4,44 +4,43 @@ declare(strict_types=1);
 
 namespace Yormy\TripwireLaravel\Services\Resolvers;
 
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
-use Mexion\BedrockUsersv2\Domain\User\Models\Admin;
-use Mexion\BedrockUsersv2\Domain\User\Models\Member;
+use Yormy\TripwireLaravel\Tests\Setup\Models\Admin;
+use Yormy\TripwireLaravel\Tests\Setup\Models\Member;
 
 class UserResolver
 {
-    public static function getCurrent(): Authenticatable
+    public static function getCurrent(): Member | Admin | null
     {
         return Auth::user();
     }
 
-    public static function getRandom(): Authenticatable
+    public static function getRandom(): ?Member
     {
         return Member::first();
     }
 
-    public static function getMemberById(string | int $id): Authenticatable
+    public static function getMemberById(string | int $id): Member
     {
         return Member::where('id', $id)->firstOrFail();
     }
 
-    public static function getAdminById(string | int $id): Authenticatable
+    public static function getAdminById(string | int $id): Admin
     {
         return Admin::where('id', $id)->firstOrFail();
     }
 
-    public static function getMemberByXid(string | int $id): Authenticatable
+    public static function getMemberByXid(string | int $id): Member
     {
         return Member::where('xid', $id)->firstOrFail();
     }
 
-    public static function getAdminByXid(string | int $id): Authenticatable
+    public static function getAdminByXid(string | int $id): Admin
     {
         return Admin::where('xid', $id)->firstOrFail();
     }
 
-    public static function getMemberOnXId(string | int $xid): ?Authenticatable
+    public static function getMemberOnXId(string | int $xid): ?Member
     {
         return Member::where('xid', $xid)->first();
     }
