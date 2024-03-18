aa471be4487683bf5525f4569c40711dbbfe21f4
Rob <rob@prigroup.eu>
2024-03-18 08:20:36 +0100

    phpstan : stan b3 fix interface

--- src/Observers/Interfaces/LoggableEventInterface.php
+++ src/Observers/Interfaces/LoggableEventInterface.php
@@ -7,4 +7,19 @@ namespace Yormy\TripwireLaravel\Observers\Interfaces;
 interface LoggableEventInterface
 {
     public function getScore(?int $score = null): int;
+
+    public function getTriggerData(): ?string;
+
+    /**
+     * @return array<string>
+     */
+    public function getTriggerRules(): array;
+
+    public function getDebugMode(): bool;
+
+    public function getTrainingMode(): bool;
+
+    public function getComment(?string $comment = null): string;
+
+    public function getViolationText(?string $violation = null): string;
 }
