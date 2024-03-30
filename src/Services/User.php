56c857513aec2760a560a473c90fad9c603c90f2
Rob <rob@prigroup.eu>
2024-03-30 10:47:22 +0100

    remove outdated user class

--- src/Services/User.php
+++ src/Services/User.php
@@ -6,24 +6,24 @@ namespace Yormy\TripwireLaravel\Services;
 
 use Illuminate\Http\Request;
 
-class User
-{
-    public static function getId(Request $request): ?string
-    {
-        return self::get($request)?->id;
-    }
-
-    public static function getType(Request $request): ?string
-    {
-        if (! self::get($request)) {
-            return null;
-        }
-
-        return get_class(self::get($request));
-    }
-
-    private static function get(Request $request): mixed
-    {
-        return $request->user();
-    }
-}
+//class User
+//{
+//    public static function getId(Request $request): ?string
+//    {
+//        return self::get($request)?->id;
+//    }
+//
+//    public static function getType(Request $request): ?string
+//    {
+//        if (! self::get($request)) {
+//            return null;
+//        }
+//
+//        return get_class(self::get($request));
+//    }
+//
+//    private static function get(Request $request): mixed
+//    {
+//        return $request->user();
+//    }
+//}
